fe27f2e62096c003965f27d9e52a8303b11e467d
1020983630 <1020983630@qq.com>
2018-04-24 22:14:05 +0800

    vip

---
+++ src/main/java/com/example/vip/web/handler/LoginHandler.java
@@ -0,0 +1,13 @@
+package com.example.vip.web.handler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginHandler {
+    @RequestMapping("/login.do")
+    public String login(String account, String password) {
+        System.out.println("我进来了");
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: creator
+  Date: 18-4-24
+  Time: 下午9:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>欢迎首页</title>
+</head>
+<body>
+    成功！
+</body>
+</html>
